616ebea877fb41a62400d3f3e4b94a27ca0e43f1
Your Name <you@example.com>
2021-07-12 21:52:00 +0100

    callback management with replay possible after video finished

--- btn_event.py
+++ btn_event.py
@@ -3,11 +3,15 @@
 import signal
 import sys
 import RPi.GPIO as GPIO
-from time import sleep
+from time import *
+import time as t
 from subprocess import call
 
+LED_GPIO = 26
 BUTTON_GPIO = 5
 ARDUINO_GPIO = 6
+
+VIDEO_DURATION_S = 55   # duration of the video to play in seconds
 
 def signal_handler(sig, frame):
     GPIO.cleanup() 
@@ -15,17 +19,37 @@ def signal_handler(sig, frame):
 
 def input_falling_callback(channel):
     print("Button pressed!")
+    time_now = t.time()
+    
+    # clear playing flag to relaunch video if its duration is reached
+    if (input_falling_callback.video_is_playing == 1) and (time_now > (input_falling_callback.time_start + VIDEO_DURATION_S)) :
+        input_falling_callback.video_is_playing = 0
+    
     if input_falling_callback.video_is_playing == 0 :
         input_falling_callback.video_is_playing = 1
+        input_falling_callback.time_start = time_now
+        
+        input_falling_callback.led_state = not input_falling_callback.led_state
+        GPIO.output(LED_GPIO, input_falling_callback.led_state)
+
         rc = call("./videoplayer.sh")
         rc = call("./offScreen.sh")
+        
+        input_falling_callback.led_state = not input_falling_callback.led_state
+        GPIO.output(LED_GPIO, input_falling_callback.led_state)
+        
 
+# member variables of input_falling_callback() fun
 input_falling_callback.video_is_playing = 0
+input_falling_callback.time_start = 0
+input_falling_callback.led_state = False
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(ARDUINO_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BUTTON_GPIO, GPIO.IN)    #external pullup 2k
+    GPIO.setup(ARDUINO_GPIO, GPIO.IN)   #external pullup 2k
+    GPIO.setup(LED_GPIO, GPIO.OUT)  
+    GPIO.output(LED_GPIO, input_falling_callback.led_state)
     
     sleep(10)
     rc = call("./offScreen.sh")
